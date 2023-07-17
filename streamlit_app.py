0b1822a00df541929e75ebf5f3ef7f3b310d0f66
shaik-iliaz <139337692+shaik-iliaz@users.noreply.github.com>
2023-07-17 10:44:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not frui_choice:
+if not fruit_choice:
   streamlit.error("Please select a frui to get information.")
 else:
 #import requests
